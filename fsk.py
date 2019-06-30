1bcf30592a79385089189ffa74b2bce118bfe418
rajash <raja.a.alshawish@gmail.com>
2019-06-30 08:51:24 +0200

    Update fsk.py

--- fsk.py
+++ fsk.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import random
 from waveform import Waveform
 from message import Message
-from awgn import Noise
 
 class FSK:
     def __init__(self, DR, SR, Fc, Cs, msg_len, Eb):
@@ -16,14 +15,15 @@ class FSK:
         self.time = np.arange(0, self.sampling_interval*msg_len, self.sampling_interval)  # time space
         self.bit_energy = Eb
         self.signal_power =  DR * Eb
-        self.waveform1 = Waveform(self.signal_power,
+        self.amplitude =  np.sqrt(self.signal_power)
+        self.waveform1 = Waveform(self.amplitude,
                                self.carrier_frequency - (self.carrier_space / 2), 
                                self.time
-                              ).sin()
-        self.waveform2 = Waveform(self.signal_power,
+                              ).cos()
+        self.waveform2 = Waveform(self.amplitude,
                                self.carrier_frequency + (self.carrier_space / 2), 
                                self.time
-                              ).sin()
+                              ).cos()
         
     def modulate(self, msg):
         msg_fsk = np.zeros(len(msg))
@@ -38,15 +38,15 @@ class FSK:
         return msg_fsk
     
     def demodulate(self, msg_fsk):
-        msg = list()
+        msg = np.zeros((len(msg_fsk)//self.sampling_rate)).astype(int)  
         
         for i in range(0,len(msg_fsk),self.sampling_rate):
 
-            if(np.round(np.mean(np.round(np.abs(msg_fsk[i:i+fsk.sampling_rate] - self.waveform1[i:i+fsk.sampling_rate])))/self.signal_power) == 0):
-                msg.append(0)
+            if(np.round(np.mean(np.round(np.abs(msg_fsk[i:i+self.sampling_rate] - self.waveform1[i:i+self.sampling_rate])))/self.amplitude) == 0):
+                msg[i//self.sampling_rate] = 0
                 
-            elif(np.round(np.mean(np.round(np.abs(msg_fsk[i:i+fsk.sampling_rate] - self.waveform2[i:i+fsk.sampling_rate])))/self.signal_power) == 0):
-                msg.append(1)
+            elif(np.round(np.mean(np.round(np.abs(msg_fsk[i:i+self.sampling_rate] - self.waveform2[i:i+self.sampling_rate])))/self.amplitude) == 0):
+                msg[i//self.sampling_rate] = 1
 
         return np.array(msg)
 
@@ -84,27 +84,3 @@ if __name__ == '__main__':
     print('Recieved Demodulated Signal:',
           ' In Binary (', demodulate,
           '), Text (', Message().toText(demodulate),').')
-
-    # Noisy Channel
-    Eb_No = 6        # in Energy per bit to Noise Density
-    num_bits = len(msg_sampled)/30
-    Noise = Noise(data_rate, Eb_No)
-    transmitted_sig = fsk.modulate(msg_sampled)
-    noise_sig = Noise.data(len(msg_sampled))
-    channel_effect = transmitted_sig + noise_sig  
-    recieved_sig = fsk.demodulate(channel_effect)
-    recieved_msg = Message().toText(recieved_sig)
-    fig, ax = plt.subplots(3,1,figsize = (20,40))
-    ax[0].plot(fsk.time, transmitted_sig)
-    ax[0].set_ylabel('Transmitted Signal')
-
-    ax[1].plot(fsk.time, noise_sig)
-    ax[1].set_ylabel('Noise Signal')
-
-    ax[2].plot(fsk.time, channel_effect)
-    ax[2].set_ylabel('Recieved Signal') 
-    ax[2].set_xlabel('Time')
-
-    plt.show()
-
-    print('Sent: ', msg,'\nRecieved: ', recieved_msg)
